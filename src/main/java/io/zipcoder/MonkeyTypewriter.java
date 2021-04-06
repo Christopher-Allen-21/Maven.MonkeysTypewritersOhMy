7f74461e1cb79de52bea150cf1a3d4899c44f5b2
Chris Allen <Chris@zipcodes-MacBook-Pro-350.local>
2021-04-05 21:15:34 -0400

    Did initial setup

--- src/main/java/io/zipcoder/MonkeyTypewriter.java
+++ src/main/java/io/zipcoder/MonkeyTypewriter.java
@@ -23,6 +23,28 @@ public class MonkeyTypewriter {
         // Do all of the Monkey / Thread building here
         // For each Copier(one safe and one unsafe), create and start 5 monkeys copying the introduction to
         // A Tale Of Two Cities.
+        SafeCopier safeMonkey1 = new SafeCopier(introduction);
+        SafeCopier safeMonkey2 = new SafeCopier(introduction);
+        SafeCopier safeMonkey3 = new SafeCopier(introduction);
+        SafeCopier safeMonkey4 = new SafeCopier(introduction);
+        SafeCopier safeMonkey5 = new SafeCopier(introduction);
+
+        UnsafeCopier unsafeMonkey1 = new UnsafeCopier(introduction);
+        UnsafeCopier unsafeMonkey2 = new UnsafeCopier(introduction);
+        UnsafeCopier unsafeMonkey3 = new UnsafeCopier(introduction);
+        UnsafeCopier unsafeMonkey4 = new UnsafeCopier(introduction);
+        UnsafeCopier unsafeMonkey5 = new UnsafeCopier(introduction);
+
+        safeMonkey1.run();
+        safeMonkey2.run();
+        safeMonkey3.run();
+        safeMonkey4.run();
+        safeMonkey5.run();
+        unsafeMonkey1.run();
+        unsafeMonkey2.run();
+        unsafeMonkey3.run();
+        unsafeMonkey4.run();
+        unsafeMonkey5.run();
 
 
         // This wait is here because main is still a thread and we want the main method to print the finished copies
@@ -34,5 +56,19 @@ public class MonkeyTypewriter {
         }
 
         // Print out the copied versions here.
+        System.out.println("\nUnsafe Monkey 1: "+unsafeMonkey1.copied);
+        System.out.println("\nUnsafe Monkey 2: "+unsafeMonkey2.copied);
+        System.out.println("\nUnsafe Monkey 3: "+unsafeMonkey3.copied);
+        System.out.println("\nUnsafe Monkey 4: "+unsafeMonkey4.copied);
+        System.out.println("\nUnsafe Monkey 5: "+unsafeMonkey5.copied);
+
+        System.out.println("\nSafe Monkey 1: "+safeMonkey1.copied);
+        System.out.println("\nSafe Monkey 2: "+safeMonkey2.copied);
+        System.out.println("\nSafe Monkey 3: "+safeMonkey3.copied);
+        System.out.println("\nSafe Monkey 4: "+safeMonkey4.copied);
+        System.out.println("\nSafe Monkey 5: "+safeMonkey5.copied);
+
+
+
     }
 }
